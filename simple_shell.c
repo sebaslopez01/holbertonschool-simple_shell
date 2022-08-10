2af2e78ba5c106e55c0248f9986ff3916f0ae12e
Sebastian <sebaslopez.2002@gmail.com>
2022-08-10 10:02:05 -0700

    Final simple shell 0.1

--- simple_shell.c
+++ simple_shell.c
@@ -18,8 +18,8 @@ int main(void)
 	int status;
 	ssize_t readed_bytes;
 	extern char **environ;
-	char *buffer = NULL, *argv[2], *prompt = "#cisfun$ ", *token = NULL;
-	size_t bufsize, promptsize = 9;
+	char *buffer = NULL, *argv[1024], *prompt = "#cisfun$ ", *token = NULL;
+	size_t bufsize, promptsize = 9, i = 0;
 
 	while (1)
 	{	
@@ -31,19 +31,25 @@ int main(void)
 
 		buffer[readed_bytes - 1] = '\0';
 		token = strtok(buffer, " ");
-		argv[0] = token;
-		argv[1] = NULL;
+		if (token == NULL)
+			break;
+		while (token != NULL)
+		{
+			argv[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		argv[i] = NULL;
 
 		new_process = fork();
 		if (new_process == EOF)
 		{
 			free(buffer);
-			return(print_error("New process error"));
+			return (print_error("New process error"));
 		}
 
 		if (new_process == 0)
 		{		
-			if (execve(token, argv, environ) == EOF)
+			if (execve(argv[0], argv, environ) == EOF)
 			{
 				free(buffer);
 				return (print_error("./shell"));
