86ae8b733bad1c3f69b35c6c4dc4c691647192d3
Sebastian <sebaslopez.2002@gmail.com>
2022-08-10 08:52:18 -0700

    Improved simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -17,32 +17,49 @@ int main(void)
 	pid_t new_process;
 	int status;
 	ssize_t readed_bytes;
-	char *buffer = NULL, *argv[2];
-	size_t bufsize = 0;
+	extern char **environ;
+	char *buffer = NULL, *argv[2], *prompt = "#cisfun$ ";
+	size_t bufsize, promptsize = 9;
 
 	while (1)
-	{
-		printf("#cisfun$ ");
+	{	
+		if (isatty(STDIN_FILENO) == 1)
+			write(STDOUT_FILENO, prompt, promptsize);
 		readed_bytes = getline(&buffer, &bufsize, stdin);
-		if (readed_bytes == -1)
-			return (0);
+		if (readed_bytes == EOF)
+			break;
 
 		buffer[readed_bytes - 1] = '\0';
 		argv[0] = buffer;
 		argv[1] = NULL;
-	
+
 		new_process = fork();
 		if (new_process == EOF)
+		{
+			free(buffer);
 			return(print_error("New process error"));
+		}
 
 		if (new_process == 0)
-		{
-			if (execve(buffer, argv, NULL) == EOF)
+		{		
+			if (execve(buffer, argv, environ) == EOF)
+			{
+				free(buffer);
 				return (print_error("./shell"));
+			}
+
+			free(buffer);
+			buffer = NULL;
+			bufsize = 0;
 		}
-		else
+		else	
 			wait(&status);
+
+		if (isatty(STDIN_FILENO) == 0)
+			break;
 	}
-	
+
+	free(buffer);
+
 	return (0);
 }
