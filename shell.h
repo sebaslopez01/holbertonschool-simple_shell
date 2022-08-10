8a8f9aa0e27650a5b42ce0a80817d234faf30fad
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-09 23:26:29 -0500

    create file shell.h

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* importing */
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* types and structures */
+#define EOF (-1)
+
+/* macros valores constantes */
+
+/* prototypes */
+
+/* macros functions */
+
+#endif
