bfcc3ce8ab1ba81de80340f99381110f7b5dabed
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-15 11:10:34 -0500

    move function filter command

--- simple_shell.c
+++ simple_shell.c
@@ -1,53 +1,11 @@
 #include "shell.h"
 
-
-char *filter_cmd(char *cmd)
-{
-	char *token = NULL, *p_cmd = NULL, *p_data = _getenv("PATH"), *p_copy = NULL;
-	size_t cmd_len = _strlen(cmd);
-	
-	if (p_data == NULL)
-		return (NULL);
-	p_cmd = malloc(sizeof(char) * cmd_len + 1);
-	if (!p_cmd)
-		return (NULL);
-	p_cmd = _strcpy(p_cmd, cmd);
-	if (access(p_cmd, F_OK) == 0)
-		return (p_cmd);
-	free(p_cmd);
-		
-	p_copy = malloc(sizeof(char) * _strlen(p_data) + 1);
-	if (!p_copy)
-		return (NULL);
-	p_copy = _strcpy(p_copy, p_data);
-	token = strtok(p_copy, ":");
-	while (token != NULL)
-	{
-		p_cmd = malloc(sizeof(char) * _strlen(token) + cmd_len + 2);
-		if (!p_cmd)
-			return (NULL);
-		_strcpy(p_cmd, token);
-		_strcat(p_cmd, "/");
-		_strcat(p_cmd, cmd);
-
-		if (access(p_cmd, F_OK) == 0)
-		{
-			free(p_copy);
-			return (p_cmd);
-		}
-		token = strtok(NULL, ":");
-		free(p_cmd);
-	}
-	free(p_copy);
-	return (NULL);
-}
-
-
 /**
  * main - program that executes command line operations
  *
  * Return: 0 Success
  */
+
 int main(void)
 {
 	pid_t new_process;
